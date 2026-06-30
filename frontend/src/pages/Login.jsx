c0ec2ab969355935682335752e69e9f832f1ac72
lucasllf2608 <lucas.llf2608@gmail.com>
2026-06-30 18:25:03 -0300

    enviando a estrutura inicial do front

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+function Login(){
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+export default Login;
